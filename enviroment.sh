f01c7ebe90150b724ed344429a92712db77cdd90
atheri <corylotze@gmail.com>
2015-09-14 22:14:56 -0700

    Added pathogen & supertab.

--- enviroment.sh
+++ enviroment.sh
@@ -1,19 +1,34 @@
 #!/bin/bash
 
 # General installs
-sudo apt-get install -y vim git
+sudo apt-get install -y vim git curl
 
 # Git config
 git config --global user.name "atheri"
 git config --global user.email "corylotze@gmail.com"
 git config --global merge.tool vimdiff
 
-#sudo rm ~/.dotfiles -r
-#git clone https://github.com/atheri/dotfiles.git ~/.dotfiles
+# Download dotfiles
+sudo rm ~/.dotfiles -r
+git clone https://github.com/atheri/dotfiles.git ~/.dotfiles
 
 # Sym links
 DEST=~/.dotfiles
 DOTFILES=".bashrc .vim"
 for file in $DOTFILES; do
+    sudo rm -r ~/$file
     ln -s -f $DEST/$file ~/$file
 done
+
+# Install Pathogen
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+# Create directories for vim
+mkdir -p ~/.vim/.undo ~/.vim/.backup ~/.vim/.swap
+
+# Get wombat colorscheme
+git clone https://github.com/sheerun/vim-wombat-scheme.git ~/.vim/bundle/vim-wombat-scheme
+
+# Get supertab vim plugin
+git clone https://github.com/ervandew/supertab.git ~/.vim/bundle/supertab
