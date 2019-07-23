d12c12a79cd381121a81c087ab7ab576a0e5a4e6
atheri <corylotze@gmail.com>
2019-07-22 18:06:55 -0700

    Missing letter

--- enviroment.sh
+++ enviroment.sh
@@ -27,7 +27,7 @@ fi
 
 # oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" "" --unattended
-git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9
+git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 
 # Git config
 git config --global user.name "atheri"
