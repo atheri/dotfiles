aa382ddc63a739da28c4457dfbd137d95b1991b3
Cory Lotze <cory.lotze@redwoodmaterials.com>
2025-08-27 09:45:55 -0700

    updates

--- environment.sh
+++ environment.sh
@@ -3,12 +3,9 @@
 # General installs
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
   echo "environment.sh: LINUX"
+  echo "=== apt base ============================================================"
   sudo apt update
   sudo apt install -y vim git curl zsh tmux gcc make
-  # python build deps
-  sudo apt install build-essential libssl-dev zlib1g-dev \
-    libbz2-dev libreadline-dev libsqlite3-dev curl \
-    libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
   chsh -s "$(which zsh)"
 elif [[ "$OSTYPE" == "darwin"* ]]; then
   echo "environment.sh: MAC"
@@ -24,22 +21,27 @@ else
   exit 1
 fi
 
-# oh-my-zsh
-if [ -d ~/.oh-my-zsh ]; then rm -rf ~/.oh-my-zsh; fi
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" "" --unattended
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/.oh-my-zsh/custom/themes/powerlevel10k
-
 # Git config
+echo "=== git config =========================================================="
 git config --global user.name "Cory Lotze"
 git config --global user.email "cory.lotze@redwoodmaterials.com"
 git config --global merge.tool vimdiff
 git config --global --add difftool.prompt false
+exit 1
+
+# oh-my-zsh
+echo "=== oh-my-zsh ==========================================================="
+if [ -d ~/.oh-my-zsh ]; then rm -rf ~/.oh-my-zsh; fi
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" "" --unattended
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/.oh-my-zsh/custom/themes/powerlevel10k
 
 # Download dotfiles
+echo "=== download dotfiles ==================================================="
 if [ -d ~/.dotfiles ]; then rm -rf ~/.dotfiles; fi
 git clone git@github.com:atheri/dotfiles.git ~/.dotfiles
 
 # Sym links
+echo "=== sym links ==========================================================="
 DEST=~/.dotfiles
 DOTFILES=".vim .zshrc .p10k.zsh .k9s"
 for file in $DOTFILES; do
@@ -48,16 +50,18 @@ for file in $DOTFILES; do
 done
 
 # Setup alias file
+echo "=== alias file =========================================================="
 FILE="aliases.zsh"
 if [ -d ~/$FILE ]; then rm -rf ~/$FILE; fi
 ln -s -f "$DEST/$FILE" "$HOME/.oh-my-zsh/custom/$FILE"
 ln -s -f "$DEST/$FILE" "$HOME/.$FILE"
 
-# Create directories for vim
-mkdir -p ~/.vim/.undo ~/.vim/.backup ~/.vim/.swap
-
 # placeholder secrets file
 touch ~/.secrets.sh
+
+# Create directories for vim
+echo "=== vim config =========================================================="
+mkdir -p ~/.vim/.undo ~/.vim/.backup ~/.vim/.swap
 
 # Get wombat colorscheme
 if [ -d ~/.vim/colors ]; then rm -rf ~/.vim/colors; fi
